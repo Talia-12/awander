5c0ebe7d710b3821852b89c5736243f57d23c4a5
Talia-12 <paradox.m.burgess@gmail.com>
2023-12-16 22:11:41 +1000

    use tabs

--- src/app.rs
+++ src/app.rs
@@ -6,44 +6,44 @@ pub type AppResult<T> = std::result::Result<T, Box<dyn error::Error>>;
 /// Application.
 #[derive(Debug)]
 pub struct App {
-    /// Is the application running?
-    pub running: bool,
-    /// counter
-    pub counter: u8,
+	/// Is the application running?
+	pub running: bool,
+	/// counter
+	pub counter: u8,
 }
 
 impl Default for App {
-    fn default() -> Self {
-        Self {
-            running: true,
-            counter: 0,
-        }
-    }
+	fn default() -> Self {
+		Self {
+			running: true,
+			counter: 0,
+		}
+	}
 }
 
 impl App {
-    /// Constructs a new instance of [`App`].
-    pub fn new() -> Self {
-        Self::default()
-    }
+	/// Constructs a new instance of [`App`].
+	pub fn new() -> Self {
+		Self::default()
+	}
 
-    /// Handles the tick event of the terminal.
-    pub fn tick(&self) {}
+	/// Handles the tick event of the terminal.
+	pub fn tick(&self) {}
 
-    /// Set running to false to quit the application.
-    pub fn quit(&mut self) {
-        self.running = false;
-    }
+	/// Set running to false to quit the application.
+	pub fn quit(&mut self) {
+		self.running = false;
+	}
 
-    pub fn increment_counter(&mut self) {
-        if let Some(res) = self.counter.checked_add(1) {
-            self.counter = res;
-        }
-    }
+	pub fn increment_counter(&mut self) {
+		if let Some(res) = self.counter.checked_add(1) {
+			self.counter = res;
+		}
+	}
 
-    pub fn decrement_counter(&mut self) {
-        if let Some(res) = self.counter.checked_sub(1) {
-            self.counter = res;
-        }
-    }
+	pub fn decrement_counter(&mut self) {
+		if let Some(res) = self.counter.checked_sub(1) {
+			self.counter = res;
+		}
+	}
 }
